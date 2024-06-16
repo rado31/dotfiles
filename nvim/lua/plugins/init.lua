df47cdec21c487b6e1cddef090903af2d9ff8bd0
Dovran Rahymov <dova.jarvis@gmail.com>
2024-06-16 23:19:59 +0500

    added: markdown preview plugin

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -106,4 +106,19 @@ return {
             }
         end,
     },
+    {
+        'iamcco/markdown-preview.nvim',
+        lazy = false,
+        cmd = {
+            'MarkdownPreviewToggle',
+            'MarkdownPreview',
+            'MarkdownPreviewStop',
+        },
+        build = 'cd app && yarn install',
+        init = function()
+            vim.g.mkdp_filetypes = { 'markdown' }
+			vim.g.mkdp_theme = 'light'
+        end,
+        ft = { 'markdown' },
+    },
 }
