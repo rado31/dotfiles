30b5a910444aed82e59141e6cc48fcbac1aa6112
rado31 <dova.jarvis@gmail.com>
2024-12-24 13:57:57 +0500

    updated: fish, tmux deleted

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,9 @@
 set fish_greeting ""
 
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 alias ll "eza --long --header --icons"
 alias lla "eza --long --header --icons -a"
 alias cat "bat"
+
+set -Ux HOMEBREW_NO_AUTO_UPDATE true
