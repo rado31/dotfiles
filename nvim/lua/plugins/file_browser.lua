4fa88207480aca96bafed6753aa5396142cb316e
rado31 <dova.jarvis@gmail.com>
2025-07-30 13:14:38 +0500

    feat: `file_browser` show hidden/ignored files

--- nvim/lua/plugins/file_browser.lua
+++ nvim/lua/plugins/file_browser.lua
@@ -1,1 +1,13 @@
-return { "nvim-telescope/telescope-file-browser.nvim" }
+return {
+	"nvim-telescope/telescope-file-browser.nvim",
+	config = function()
+		require("telescope").setup({
+			extensions = {
+				file_browser = {
+					hidden = { file_browser = true, folder_browser = true },
+					no_ignore = true,
+				},
+			},
+		})
+	end,
+}
