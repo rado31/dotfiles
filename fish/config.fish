95319df57282cdfd0783a74957de657c465569cb
rado31 <dova.jarvis@gmail.com>
2025-10-01 15:08:59 +0500

    feat: updated `fish` config

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,6 @@
 set fish_greeting ""
 
-eval "$(/opt/homebrew/bin/brew shellenv)"
-set -Ux HOMEBREW_NO_AUTO_UPDATE true
+# eval "$(/opt/homebrew/bin/brew shellenv)" # for MacOS
+# set -Ux HOMEBREW_NO_AUTO_UPDATE true # for MacOS
+
+# set -Ux WEBKIT_DISABLE_DMABUF_RENDERER 1 # for tauri apps in Wayland compositor
