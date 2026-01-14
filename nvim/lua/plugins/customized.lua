78fd9146bc13e4687534d6a929dffd2c50f55678
rado31 <dova.jarvis@gmail.com>
2026-01-14 18:42:26 +0500

    feat: `nvim` updated
    - disable inline hints
    - exclude some folders in file search

--- nvim/lua/plugins/customized.lua
+++ nvim/lua/plugins/customized.lua
@@ -2,9 +2,17 @@ return {
   { "LazyVim/LazyVim", opts = { colorscheme = "everforest" } },
 
   {
+    "snacks.nvim",
+    opts = {
+      picker = { exclude = { "nodu_modules", ".git", "target" } },
+    },
+  },
+
+  {
     "neovim/nvim-lspconfig",
     ---@class PluginLspOpts
     opts = {
+      inlay_hints = { enabled = false },
       ---@type lspconfig.options
       servers = {
         rust_analyzer = {},
